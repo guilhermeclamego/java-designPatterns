4420282319dd504fa44b2b06f7e4e18f29486a9c
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-02-07 17:33:14 -0300

    Implemtação do Padrão Observer

--- src/br/com/alura/loja/TestesPedidos.java
+++ src/br/com/alura/loja/TestesPedidos.java
@@ -4,18 +4,26 @@ import br.com.alura.loja.orcamento.Orcamento;
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
 import br.com.alura.loja.pedido.Pedido;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBanco;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 public class TestesPedidos {
     public static void main(String[] args) {
-        String cliente = args[0];
-        BigDecimal valorOrcamento = new BigDecimal(args[1]);
-        int quantidadeItens = Integer.parseInt(args[2]);
+        String cliente = "Guilherme Cesar Lamego";
+        BigDecimal valorOrcamento = new BigDecimal("500");
+        int quantidadeItens = 4;
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-        GeraPedidoHandler handler = new GeraPedidoHandler(/*depedencias: repository, service...*/);
+        GeraPedidoHandler handler = new GeraPedidoHandler(
+                //Lista das ações que irão executar (Padrão observer)
+                Arrays.asList(
+                        new SalvarPedidoNoBanco(),
+                        new EnviarEmailPedido())
+        );
         handler.execute(gerador);
     }
 }
